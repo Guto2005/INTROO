bf0782d19cd287ced213c8809f3d4c1f8129b1b6
GUSTAVO DUARTE DOS SANTOS <gustavo.dsantos11@senacsp.edu.br>
2024-08-22 16:58:48 -0300

    feito o desafio do 'maior de idade'

--- app/models/Pessoa.php
+++ app/models/Pessoa.php
@@ -1,79 +1,87 @@
 <?php
 
 namespace app\models;
+
+use DateTime;
+use DateInterval;
+
 class Pessoa {
 
-  public string $nome;
+    public string $nome;
+    public string $cpf;
+    private DateTime $dataNascimento;
 
-  public string $cpf;
+    public function __construct(string $pNome, string $pCpf, DateTime $pDataNascimento) {
+        $this->setNome($pNome);
+        $this->setCpf($pCpf);
+        $this->dataNascimento = $pDataNascimento;
+    }
 
-  public int $idade;
+    /**
+     * Get the value of idade
+     */ 
+    public function getIdade(): int {
+        $hoje = new DateTime();
+        $idade = $hoje->diff($this->dataNascimento)->y;
+        return $idade;
+    }
 
-  public function __construct(string $pNome, string $pCpf, int $pIdade){
-    $this->setNome($pNome);
-    $this->setCpf($pCpf);
-    $this->setIdade($pIdade);
-  }
+    /**
+     * Check if the person is of legal age
+     */
+    public function maiorDeIdade(): bool {
+        $hoje = new DateTime();
+        $dataLimite = new DateTime('18 years ago');
+        return $this->dataNascimento <= $dataLimite;
+    }
 
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome(): string {
+        return $this->nome;
+    }
 
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */ 
+    public function setNome(string $nome): self {
+        $this->nome = $nome;
+        return $this;
+    }
 
-  /**
-   * Get the value of idade
-   */ 
-  public function getIdade()
-  {
-    return $this->idade;
-  }
+    /**
+     * Get the value of cpf
+     */ 
+    public function getCpf(): string {
+        return $this->cpf;
+    }
 
-  /**
-   * Set the value of idade
-   *
-   * @return  self
-   */ 
-  public function setIdade($idade)
-  {
-    $this->idade = $idade;
+    /**
+     * Set the value of cpf
+     *
+     * @return  self
+     */ 
+    private function setCpf(string $cpf): self {
+        $this->cpf = $cpf;
+        return $this;
+    }
 
-    return $this;
-  }
+    /**
+     * Get the value of dataNascimento
+     */ 
+    public function getDataNascimento(): string {
+        return $this->dataNascimento->format('d/m/Y');
+    }
 
-  /**
-   * Get the value of nome
-   */ 
-  public function getNome()
-  {
-    return $this->nome;
-  }
-
-  /**
-   * Set the value of nome
-   *
-   * @return  self
-   */ 
-  public function setNome($nome)
-  {
-    $this->nome = $nome;
-
-    return $this;
-  }
-
-  /**
-   * Get the value of cpf
-   */ 
-  public function getCpf()
-  {
-    return $this->cpf;
-  }
-
-  /**
-   * Set the value of cpf
-   *
-   * @return  self
-   */ 
-  private function setCpf($cpf)
-  {
-    $this->cpf = $cpf;
-
-    return $this;
-  }
+    public function exibirDadosPessoa(): string {
+        return "Nome: " . $this->getNome() . "; " .
+               "CPF: " . $this->getCpf() . "; " .
+               "Data de Nascimento: " . $this->getDataNascimento() . "; " .
+               "Idade: " . $this->getIdade() . " anos; " .
+               "Maior de idade: " . ($this->maiorDeIdade() ? 'Sim' : 'Não');
+    }
 }
+?>
--- app/models/Produto.php
+++ app/models/Produto.php
@@ -20,7 +20,7 @@ class Produto{
 
     public function setValor(float $pValor){
         if ($pValor<=0) {
-            throw new Exception;
+            throw new Exception("Valor inválido - O valor deve ser maior que zero");
         }
         $this->valor = $pValor;
     }
@@ -29,7 +29,32 @@ class Produto{
     }
 
     public function getValor(){
-        return $this->valor;
+        return number_format($this->valor,2);
+    }
+
+    public function exibirDadosProduto():string{
+        $mensagem = "Produto: " . $this->getNome() . ", Valor(R$)" . $this->getValor();
+        return $mensagem;
+    }
+
+    /* Criar um método que aplique descontos aos produtos de acordo com a seguinte regra:
+    - Produtos que custam mais que R$ 100 e R$ 1000 - desconto de 5%
+    -Produtos que custam menos de R$ 100 não aplicar desconto
+    */
+
+    public function retornarDesconto():float{
+        $desconto=0;
+        if ($this->valor>100 && $this->valor<=1000) {
+            $desconto = 0.05;
+        } elseif ($this->valor>1000) {
+            $desconto = 0.1;
+        }
+
+        return $this->valor*$desconto; 
+    }
+    public function retornarPrecoDesconto():float{
+        return $this->valor-$this->retornarDesconto(); 
     }
 }
+
 ?>
